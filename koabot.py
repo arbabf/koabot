a044070a66a5be9e6117fe9261cefa46a8fd2386
Arbab Ahmed <arbab.fahm@gmail.com>
2021-06-12 22:22:35 +1000

    Finished up the rest of the functionality.

--- koabot.py
+++ koabot.py
@@ -1,4 +1,5 @@
 # The Kingdoms of Atlantica bot.
+# Author: arbabf
 
 import os
 import discord
@@ -15,13 +16,56 @@ bot.remove_command("help")
 
 
 class rollStuff(commands.Cog):
-    def success(self, arg_string, mods):
-        pass
+    def success(self, dice, mods, thresh) -> str:
+        ret_str = ""
+        for i in range(mods[2]):
+            arr = self.roll(dice, mods)
+            roll_str = str(arr[1][0])
+            for elem in arr[1][1:]:
+                roll_str = "".join([roll_str, ", " + str(elem)])
+            if arr[0] > thresh:
+                if i == 0:
+                    ret_str = "".join([ret_str, ("succeeded. Threshold was {0}; roll was {1}. [{2}, total modifier {3}].").format(str(thresh), arr[0], roll_str, str(mods[1]))])
+                else:
+                    ret_str = "".join([ret_str, ("\n Roll {0} has succeeded. Threshold was {1}; roll was {2}. [{3}, total modifier {4}].").format(str(i+1), str(thresh), arr[0], roll_str, str(mods[1]))])
+            else:
+                if i == 0:
+                    ret_str = "".join([ret_str, ("failed. Threshold was {0}; roll was {1}. [{2}, total modifier {3}].").format(str(thresh), arr[0], roll_str, str(mods[1]))])
+                else:
+                    ret_str = "".join([ret_str, ("\n Roll {0} has failed. Threshold was {1}; roll was {2}. [{3}, total modifier {4}].").format(str(i+1), str(thresh), arr[0], roll_str, str(mods[1]))])
+        return ret_str
 
-    def count_successes(self, arg_string, mods):
-        pass
+    def count_successes(self, dice, mods, thresh) -> str:
+        ret_str = ""
+        for i in range(mods[2]):
+            succ_rolls = 0
+            arr = self.roll(dice, mods)
+            for elem in arr[1]:
+                if elem + mods[1] > thresh:
+                    succ_rolls += 1
+            roll_str = str(arr[1][0])
+            for elem in arr[1][1:]:
+                roll_str = "".join([roll_str, ", " + str(elem)])
+            if i == 0:
+                ret_str = "".join([ret_str, ("succeeded {0} times. Threshold was {1}. [{2}, total modifier {3}].").format(str(succ_rolls), str(thresh), roll_str, str(mods[1]))])
+            else:
+                ret_str = "".join([ret_str, ("\nAttempt {0} succeeded {1} times. Threshold was {2}. [{3}, total modifier {4}].").format(str(i+1), str(succ_rolls), str(thresh), roll_str, str(mods[1]))])
+        return ret_str
 
-    def roll(self, dice, mods):
+    def single_roll(self, dice, mods, thresh) -> str:
+        ret_str = ""
+        for i in range(mods[2]):
+            arr = self.roll(dice, mods)
+            roll_str = str(arr[1][0])
+            for elem in arr[1][1:]:
+                roll_str = "".join([roll_str, ", " + str(elem)])
+            if i == 0:
+                ret_str = "".join("rolled {0}. [{1}, total modifier {2}].").format(arr[0], roll_str, str(mods[1]))
+            else:
+                ret_str = "".join([ret_str, ("\nAlso rolled {0}. [{1}, total modifier {2}].").format(arr[0], roll_str, str(mods[1]))])
+        return ret_str
+    
+    def roll(self, dice, mods) -> list:
         # chucks a roll w/ modifiers if there are any
         sum = 0
         roll_results = []
@@ -29,8 +73,10 @@ class rollStuff(commands.Cog):
             num = random.randint(1, dice)
             roll_results.append(num)
             sum += num
+        sum += mods[1]
+        return [sum, roll_results]
 
-    def parse_args(self, arg_string, ctx):
+    def parse_args(self, arg_string):
         # does the actual parsing
         # returns: a tuple consisting of count and optional +- and * modifiers
         count = 1
@@ -54,22 +100,51 @@ class rollStuff(commands.Cog):
         except Exception:
             return (-1, None, None)
         indice = arg_string.find('d')+1
+        while indice != len(arg_string) and arg_string[indice] in string.digits:
+            indice += 1 #stupid workaround, fix later
         while indice != len(arg_string) and arg_string[indice] != '*':
-            pass
-        return (count, None, None)
+            # look for every +/- modifier
+            if arg_string[indice] in ['+', '-']:
+                num = 0
+                indice += 1
+                old_index = indice-1
+                while indice != len(arg_string) and arg_string[indice] in string.digits:
+                    num = num * 10 + int(arg_string[indice])
+                    indice += 1
+                if indice != len(arg_string) and arg_string[indice] not in string.digits and not (arg_string[indice] in ['+', '-', '*', '>']):
+                    # error checking - if we have a non-numeric char or an operand just immediately terminate
+                    return (-1, None, None)
+                plus_minus_mods.append(num)
+                plus_minus_mods[len(plus_minus_mods)-1] += 2 * plus_minus_mods[len(plus_minus_mods)-1] * (int(arg_string[old_index] == '+') - 1) # oneliner to negate number if -, but do nothing if +
+            elif arg_string[indice] == '>':
+                break
+            else:
+                return (-1, None, None)
+        total = 0
+        mult_count = 1
+        for elem in plus_minus_mods:
+            total += elem
+        if indice != len(arg_string) and arg_string[indice] == '*':
+            # look for how many times we should repeat this roll
+            indice += 1
+            num = 0
+            while indice != len(arg_string) and arg_string[indice] in string.digits:
+                num = num * 10 + int(arg_string[indice])
+                indice += 1
+            if indice != len(arg_string) and arg_string[indice] not in string.digits and not arg_string[indice] == '>':
+                # error checking
+                return(-1, None, None)
+            mult_count = num
+        return (count, total, mult_count, plus_minus_mods)
 
 
     @commands.command(name='roll')
     async def roll_parse(self, ctx, *args):
         # parses commands given through !roll command
+        # only parses dice, gt
         arg_string = ''
         for arg in args:
             arg_string += arg # get the argument string into one contiguous string which we can then parse
-        gt_count = 0
-        for char in arg_string:
-            # count gt symbols
-            if char == '>':
-                gt_count += 1
         # get dice size
         indice = arg_string.find('d')+1
         if indice == 0:
@@ -83,20 +158,36 @@ class rollStuff(commands.Cog):
         if dice == 0:
             await ctx.send("Number of faces must be greater than 0!")
             return
-        if (gt_count >= 3):
-            await ctx.send("Invalid format.")
+        gt_count = 0
+        gt_index = 0
+        last_gt = len(arg_string)
+        for char in arg_string:
+            # count gt symbols
+            if char == '>':
+                last_gt = gt_index
+                gt_count += 1
+            gt_index += 1
+        indice = last_gt + 1
+        thresh = 0
+        while indice < len(arg_string) and arg_string[indice] in string.digits:
+            # get threshold for gt
+            thresh = thresh * 10 + int(arg_string[indice])
+            indice += 1
+        if indice < len(arg_string) and arg_string[indice] not in string.digits:
+            await ctx.send("Invalid format. Check !help for a list of allowed formats.")
             return
-
-        tup = self.parse_args(arg_string, ctx)
+        if (gt_count >= 3):
+            await ctx.send("Invalid format. Check !help for a list of allowed formats.")
+            return
+        tup = self.parse_args(arg_string)
         if tup[0] == -1:
             await ctx.send("Invalid format. Check !help for a list of allowed formats.")
             return
-        await ctx.send([
-            # switch statement, sort of (this has no right to exist)
-            self.roll(dice, tup),
-            self.success(dice, tup),
-            self.count_successes(dice, tup)
-        ][gt_count])
+        if gt_count == 2 and tup[0] > 1 and len(tup[3]) > 0:
+            await ctx.send("Invalid format. Check !help for a list of allowed formats.")
+            return
+        # switch statement, kinda????
+        await ctx.send(ctx.message.author.mention + " has " + [self.single_roll, self.success, self.count_successes][gt_count](dice, tup, thresh))
 
 @bot.command(name='flip')
 async def flip(ctx):
@@ -109,33 +200,36 @@ async def flip(ctx):
 @bot.command(name='help')
 async def help(ctx):
     await ctx.send("""```Kingdoms of Atlantica bot. Made to be an improvement over RPBot, tuned to our specific group's needs.\n
-    Commands:\n
-    !help - brings up this help text\n
-    !roll - rolls dice. Parameters:\n
-    <optional number of dice> d <number of faces> <optional +- modifiers> <optional repetition modifier>\n
-    Optional: Use > to sum up each individual dice roll, or >> to count successes.\n
-    Example rolls:\n
-    - !roll d20\n
-    - !roll 3d6\n
-    - !roll d20 > 12\n
-    - !roll 2d20 + 2 >> 12\n
-    - !roll 3d20 - 2 + 3 * 5 >> 15 (* modifier rolls (3d20 - 2 + 3) 5 times)\n
-    !flip - flips a coin\n
+Commands:
+    !help - brings up this help text
+    !roll - rolls dice. Parameters:
+    <optional number of dice> d <number of faces> <optional +- modifiers> <optional repetition modifier>
+    Optional: Use > to sum up each individual dice roll, or >> to count successes.
+    Example rolls:
+    - !roll d20
+    - !roll 3d6
+    - !roll d20 > 12
+    - !roll 2d20 + 2 >> 12
+    - !roll d20 - 2 + 3 * 5 >> 15 (* modifier rolls (d20 - 2 + 3) 5 times)
+    NOTE: >> cannot take more than one die. Use * to remedy this.
+    !flip - flips a coin
     !pref - change command prefix```""")
 
 @bot.command(name='pref')
 async def pref(ctx):
-    await ctx.send("Type in the prefix you would like (max 10 characters):")
-    def check(msg):
-        if len(msg.content) <= 10:
-            return msg
-    try:
-        new_pref = await bot.wait_for("message", check=check, timeout=60.0)
-    except asyncio.TimeoutError:
-        await ctx.send("Taken too long to set a prefix!")
+    if ctx.message.author.guild_permissions.administrator:
+        await ctx.send("Type in the prefix you would like (max 10 characters):")
+        def check(msg):
+            if len(msg.content) <= 10 and msg.author == ctx.message.author:
+                return msg
+        try:
+            new_pref = await bot.wait_for("message", check=check, timeout=60.0)
+        except asyncio.TimeoutError:
+            await ctx.send("Taken too long to set a prefix!")
+        else:
+            await ctx.send("New prefix set to {0}.".format(new_pref.content))
+            bot.command_prefix = new_pref.content
     else:
-        await ctx.send("New prefix set to {0}.".format(new_pref.content))
-        bot.command_prefix = new_pref.content
-
+        await ctx.send("Only a user with administrator permissions can perform this command.")
 bot.add_cog(rollStuff(bot))
 bot.run(TOKEN)
